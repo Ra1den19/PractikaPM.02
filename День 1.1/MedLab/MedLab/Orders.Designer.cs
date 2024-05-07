b24c07749bf33c6dfb21df6189d3f90b601bb9ca
Ra1den19 <162029818+Ra1den19@users.noreply.github.com>
2024-05-07 15:04:00 +0300

    День 1.1
    -Обновлено приложение MedLab

---
+++ День 1.1/MedLab/MedLab/Orders.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MedLab
+{
+    partial class Orders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Show_Orders = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(665, 242);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Show_Orders
+            // 
+            this.Show_Orders.Location = new System.Drawing.Point(13, 278);
+            this.Show_Orders.Name = "Show_Orders";
+            this.Show_Orders.Size = new System.Drawing.Size(147, 55);
+            this.Show_Orders.TabIndex = 1;
+            this.Show_Orders.Text = "Показать заказы";
+            this.Show_Orders.UseVisualStyleBackColor = true;
+            this.Show_Orders.Click += new System.EventHandler(this.Show_Orders_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(491, 320);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 209);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Информация";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Показать список заказов";
+            // 
+            // Orders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 572);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Show_Orders);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Orders";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Orders";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Show_Orders;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ День 1.1/MedLab/MedLab/Orders.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedLab
+{
+    public partial class Orders : Form
+    {
+        public Orders()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Orders_Click(object sender, EventArgs e)
+        {
+            string connectionString = $@"Data Source = DESKTOP-BAT1MFP\SQLEXPRESS; Initial Catalog = MedicalLaboratory; Integrated Security = True";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "select * from Заказы";
+                SqlCommand com = new SqlCommand(query, con);
+                SqlDataAdapter ad = new SqlDataAdapter(com);
+                DataTable table= new DataTable();
+                ad.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
+    }
+}
